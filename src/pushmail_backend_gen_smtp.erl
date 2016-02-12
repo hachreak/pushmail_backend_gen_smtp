6dd5b224df15b2a35acd6f5513fda72b96099ce8
Leonardo Rossi <leonardo.rossi@studenti.unipr.it>
2016-02-12 20:34:06 +0100

    global: fix library
    Signed-off-by: Leonardo Rossi <leonardo.rossi@studenti.unipr.it>

--- src/pushmail_backend_gen_smtp.erl
+++ src/pushmail_backend_gen_smtp.erl
@@ -30,11 +30,13 @@
 -spec start() ->
   {ok, pushmail:appctx()} | {error: term()}.
 start() ->
-  application:get_env(pushmail, backend_gen_smtp).
+  {ok, AppCtx} = application:get_env(pushmail, pushmail_backend_gen_smtp),
+  start(AppCtx).
 
 -spec start(pushmail:appctx()) ->
   {ok, pushmail:appctx()} | {error: term()}.
 start(AppCtx) ->
+  start_ssl_if_needed(AppCtx),
   {ok, AppCtx}.
 
 -spec stop(pushmail:appctx()) -> ok | {error: term()}.
@@ -46,7 +48,7 @@ stop(_) ->
   {ok, pushmail:appctx()} | {error, term()}.
 send(#{sender := Sender, receivers := Receivers}=Mail, AppCtx) ->
   EncodedEmail = encode_body(Mail),
-  gen_smtp:send({Sender, Receivers, EncodedEmail}, AppCtx, wait_reply),
+  gen_smtp_client:send({Sender, Receivers, EncodedEmail}, AppCtx, wait_reply),
   {ok, AppCtx}.
 
 -spec encode_body(pushmail:mail()) -> binary().
@@ -54,15 +56,18 @@ encode_body(#{sender := Sender, receivers := Receivers, subject := Subject,
               message := Message}) ->
   From = {<<"From">>, Sender},
   To = {<<"To">>, binary_join(Receivers, <<", ">>)},
-  Subject = {<<"Subject">>, Subject},
-  mimemail:encode({<<"text">>, <<"plain">>, [From, To, Subject], [], Message}).
+  EmailSubject = {<<"Subject">>, Subject},
+  mimemail:encode(
+    {<<"text">>, <<"plain">>, [From, To, EmailSubject], [], Message}).
 
 
 -spec binary_join(list(), binary() | string()) -> binary().
+binary_join([], _Separator) -> <<"">>;
+binary_join([Binary], _Separator) when is_binary(Binary) -> Binary;
 binary_join(Binary, _Separator) when is_binary(Binary) ->
   Binary;
 binary_join([Head | Tail], Separator) ->
-  list:foldl(fun(Value, Acc) ->
+  lists:foldl(fun(Value, Acc) ->
                  << Acc/binary, Separator/binary, Value/binary >>
              end, Head, Tail).
 
@@ -85,3 +90,10 @@ wait_reply({exit, ExitReason}) ->
     {plugin, backend_gen_smtp},
     {error, ExitReason}
   }).
+
+start_ssl_if_needed(AppCtx) ->
+  case proplists:get_value(ssl, AppCtx) of
+    undefined -> ok;
+    false -> ok;
+    true -> application:ensure_all_started(ssl)
+  end.
